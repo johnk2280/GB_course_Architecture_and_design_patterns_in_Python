d3c8cf7425ef9b7aab1e3039982a947f767e9f17
johnk2280 <johnk2280@gmail.com>
2022-05-21 00:55:16 +0500

    [upd] manage.py

--- framework/server.py
+++ framework/server.py
@@ -1,15 +1,37 @@
 import socket
 from socket import AF_INET
 from socket import SOCK_STREAM
+from typing import Tuple
+
+import argparse
+
+
+def parse_commandline() -> Tuple[str, int]:
+    addr = '127.0.0.1'
+    port = 8000
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            'port',
+            type=str,
+        )
+        args = parser.parse_args()
+        print(args)
+        addr = args.address
+        port = int(args.port)
+    except AttributeError:
+        pass
+
+    return addr, port
+
 
 server_sock = socket.socket(AF_INET, SOCK_STREAM)
-server_address = ('127.0.0.1', 8002)
+server_address = parse_commandline()
 server_sock.bind(server_address)
 server_sock.listen(5)
 
-
 while True:
-    print('Сервер готов к работе на адресе http://127.0.0.1:8002')
+    print(f'Сервер готов к работе на адресе {server_address}')
     client, address = server_sock.accept()
     print(f'Подключился {address}')
     data = client.recv(1024).decode()
