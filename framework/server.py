dd3aa983f6bb93a110d24c7be82f1e7e6efae22c
johnk2280 <johnk2280@gmail.com>
2022-05-20 21:03:51 +0500

    [add] server

---
+++ framework/server.py
@@ -0,0 +1,18 @@
+import socket
+from socket import AF_INET
+from socket import SOCK_STREAM
+
+
+server_sock = socket.socket(AF_INET, SOCK_STREAM)
+server_address = ('127.0.0.1', 8002)
+server_sock.bind(server_address)
+server_sock.listen(5)
+
+while True:
+    print('Сервер готов к работе')
+    client, address = server_sock.accept()
+    print(f'Подключился {address}')
+    data = client.recv(1024).decode()
+    print(f'Получены данные {data}')
+    client.sendall(b'Hello from my server\n')
+    client.close()
