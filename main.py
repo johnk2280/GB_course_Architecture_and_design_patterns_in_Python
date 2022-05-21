e5c08fb7772af629f647201f1c22b955aa2bd8fa
johnk2280 <johnk2280@gmail.com>
2022-05-21 13:53:20 +0500

    [add] response

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from framework.wsgi import Framework
 from framework.url import Url
 from framework.view import View
+from framework.response import Response
 
 
 class MyFirstView(View):
 
-    def get(self, request, *args, **kwargs):
-        return 'Я ГЕТ запрос'
+    def get(self, request, *args, **kwargs) -> Response:
+        return Response(status=200, body='GET SUCCESS')
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs) -> Response:
         return 'Я ПОСТ запрос'
 
 
