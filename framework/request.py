2f61b73942100cfd11c0739008ddedb6c9dd3d10
johnk2280 <johnk2280@gmail.com>
2022-06-08 21:16:47 +0500

    [add] start hw_6

--- framework/request.py
+++ framework/request.py
@@ -1,7 +1,6 @@
 class Request:
 
     def __init__(self, environ: dict):
-
         self.environ = environ
         self.method = environ.get('REQUEST_METHOD').lower()
         self.path = environ.get('PATH_INFO')
