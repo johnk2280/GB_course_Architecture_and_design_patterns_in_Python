2025747b6923376ee8d15e78b492b6a2b12bc382
johnk2280 <johnk2280@gmail.com>
2022-06-12 23:54:34 +0500

    [upd] contacts.html

--- project/framework/abstracts/abstract_view.py
+++ project/framework/abstracts/abstract_view.py
@@ -6,17 +6,17 @@ from framework.response import Response
 class View(ABC):
 
     def get(self, request, *args, **kwargs) -> Response:
-        pass
+        return Response(status='405 Method Not Allowed', body='405.html')
 
     def post(self, request, *args, **kwargs) -> Response:
-        pass
+        return Response(status='405 Method Not Allowed', body='405.html')
 
-    # def put(self, request, *args, **kwargs) -> Response:
-    #     pass
-    #
-    # def delete(self, request, *args, **kwargs) -> Response:
-    #     pass
-    #
-    # def update(self, request, *args, **kwargs) -> Response:
-    #     pass
+    def put(self, request, *args, **kwargs) -> Response:
+        return Response(status='405 Method Not Allowed', body='405.html')
+
+    def delete(self, request, *args, **kwargs) -> Response:
+        return Response(status='405 Method Not Allowed', body='405.html')
+
+    def update(self, request, *args, **kwargs) -> Response:
+        return Response(status='405 Method Not Allowed', body='405.html')
 
